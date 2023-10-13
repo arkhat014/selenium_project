67c0fe6b4afd928685ccfab82cf1f4f9df163505
arkhat014 <119475457+arkhat014@users.noreply.github.com>
2023-10-13 16:37:40 +0600

    Newmain.py

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver import FirefoxOptions
+from datetime import date, timedelta, datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
 import time
+
+args = {
+    'owner': 'alit.albin',
+    'start_date': datetime(2023, 10, 10),
+}
 
 def get_driver():
     options = {
@@ -46,3 +54,14 @@ def check_application2(application):
         return True
     except:
         return False
+
+def stat_gov_v2():
+    application = get_application()
+    result = check_application2(application)
+    return result
+
+with DAG('stat_gov_v2', description='stat_gov_v2', schedule_interval='*/12 * * * *', catchup=False,
+             default_args=args) as dag:
+        get_statgov_v2 = PythonOperator(task_id='get_statgov_v2', python_callable=get_statgov_v2)
+        get_statgov_v2
+
